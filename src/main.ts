d1c1e5f8dc346330ee9243d141047d9187edce62
Simon LEONARD <git-1001af4@sinux.sh>
2022-12-01 21:01:34 +0000

    feat: add swagger for testing/documenting api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix("api");
+
+  const config = new DocumentBuilder()
+    .setTitle("Full-remote API")
+    .setDescription("API for #NuitDeLInfo")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("swagger", app, document);
+
   await app.listen(3000);
 }
 bootstrap();
