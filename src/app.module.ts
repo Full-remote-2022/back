a97535e4e2dc29af4b91810449c1f5bf717bb94a
Simon LEONARD <git-1001af4@sinux.sh>
2022-12-01 22:58:30 +0000

    chore: setup mongoose and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+
+import config from "./config/configuration";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      connectionName: "db",
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>("dbUri"),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
